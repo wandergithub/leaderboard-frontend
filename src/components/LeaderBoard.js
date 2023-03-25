47307a0b3a439c3c4ac653fca9ad063bf18ad11b
wandergithub <wandergonzalez0@hotmail.com>
2023-03-25 17:42:59 -0400

    Add react routes dom

---
+++ src/components/LeaderBoard.js
@@ -0,0 +1,12 @@
+const LeaderBoard = () => {
+    return (
+        <div>
+            <ul>
+                <li>wander</li>
+            </ul>
+        </div>
+    );
+};
+
+
+export default LeaderBoard;
