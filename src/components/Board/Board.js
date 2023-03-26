38beddad50133657520414e3bb01ed500d26ed46
wandergithub <wandergonzalez0@hotmail.com>
2023-03-25 21:14:51 -0400

    Design board items style

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,12 +1,30 @@
-const Board = () => {
-    return (
-        <div>
-            <ul className="list-group">
-                <li className="list-group-item">wander</li>
-            </ul>
-        </div>
-    );
-};
+import place_1 from "./Assets/1st-place-medal-svgrepo-com.svg"
+import place_2 from "./Assets/2nd-place-medal-svgrepo-com.svg"
+import place_3 from "./Assets/3rd-place-medal-svgrepo-com.svg"
 
+const Board = () => {
+  return (
+    <div className="m-5">
+      <ul className="list-group">
+        <li className="list-group-item d-flex justify-content-between">
+          <span><img src={place_1} width="25px" />wander</span>
+          <span>Points: 2100</span>{" "}
+        </li>
+        <li className="list-group-item d-flex justify-content-between">
+          <span><img src={place_2} width="25px" />Pedro</span>
+          <span>Points: 2100</span>{" "}
+        </li>
+        <li className="list-group-item d-flex justify-content-between">
+          <span><img src={place_3} width="25px" />Fulano</span>
+          <span>Points: 2100</span>{" "}
+        </li>
+        <li className="list-group-item d-flex justify-content-between">
+          <span>Fulano</span>
+          <span>Points: 2100</span>{" "}
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Board;
