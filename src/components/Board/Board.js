5433fcfa42b39596ab1ec758666929b391bc7107
wandergithub <wandergonzalez0@hotmail.com>
2023-03-25 21:18:21 -0400

    Add race stats button

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,21 +1,30 @@
-import place_1 from "./Assets/1st-place-medal-svgrepo-com.svg"
-import place_2 from "./Assets/2nd-place-medal-svgrepo-com.svg"
-import place_3 from "./Assets/3rd-place-medal-svgrepo-com.svg"
+import place_1 from "./Assets/1st-place-medal-svgrepo-com.svg";
+import place_2 from "./Assets/2nd-place-medal-svgrepo-com.svg";
+import place_3 from "./Assets/3rd-place-medal-svgrepo-com.svg";
 
 const Board = () => {
   return (
     <div className="m-5">
       <ul className="list-group">
         <li className="list-group-item d-flex justify-content-between">
-          <span><img src={place_1} width="25px" />wander</span>
+          <span>
+            <img src={place_1} width="25px" />
+            wander
+          </span>
           <span>Points: 2100</span>{" "}
         </li>
         <li className="list-group-item d-flex justify-content-between">
-          <span><img src={place_2} width="25px" />Pedro</span>
+          <span>
+            <img src={place_2} width="25px" />
+            Pedro
+          </span>
           <span>Points: 2100</span>{" "}
         </li>
         <li className="list-group-item d-flex justify-content-between">
-          <span><img src={place_3} width="25px" />Fulano</span>
+          <span>
+            <img src={place_3} width="25px" />
+            Fulano
+          </span>
           <span>Points: 2100</span>{" "}
         </li>
         <li className="list-group-item d-flex justify-content-between">
@@ -23,6 +32,7 @@ const Board = () => {
           <span>Points: 2100</span>{" "}
         </li>
       </ul>
+      <button type="button" class="btn btn-primary mt-3">Add race stats</button>
     </div>
   );
 };
