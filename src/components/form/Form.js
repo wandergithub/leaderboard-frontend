11893d7700911caed58d7a1bcb97ebf0e66ef917
wandergithub <wandergonzalez0@hotmail.com>
2023-03-28 12:25:08 -0400

    Add form functions to handle form change and submition to PlayerCreateForm

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,16 +1,33 @@
+import { useDispatch, useSelector } from "react-redux";
+
 export function PlayerCreateForm() {
-  const createPlayer = (e) => {
-    
+  const [name, setName] = useSelector("");
+  const dispatch = useDispatch();
+  //   Form actions
+  const handleChange = (e) => {
+    const input = e.target;
+    switch (input.name) {
+      case "name":
+        setName(input.value);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const handleSubmit = () => {
+    dispatch();
   };
 
   return (
     <>
       <h2 className="m-5">Add new player</h2>
-      <form className="d-flex flex-column m-5" onChange={handleForm}>
+      <form className="d-flex flex-column m-5" onChange={handleChange}>
         <label for="name" className="form-label mt-3">
           Name:
         </label>
-        <input type={"text"} id="name" name="name" />
+        <input type={"text"} id="name" name="name" value={name} />
 
         <button type="submit" className="btn btn-success mt-3">
           Create
