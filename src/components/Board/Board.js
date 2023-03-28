052024d8d836d6866dfc38d404dc6e4427d969d8
wandergithub <wandergonzalez0@hotmail.com>
2023-03-27 20:10:42 -0400

    display players from redux store with useSelector on Board component

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,24 +1,22 @@
-import { useState } from "react";
 import { useSelector } from "react-redux";
 import place_1 from "./Assets/1st-place-medal-svgrepo-com.svg";
 import place_2 from "./Assets/2nd-place-medal-svgrepo-com.svg";
 import place_3 from "./Assets/3rd-place-medal-svgrepo-com.svg";
 
 const Board = () => {
-  const [players, setPlayers] = useState(useSelector((state) => state.players));
+  const players = useSelector((state) => state.players);
+
   return (
     <div className="m-5">
       <ul className="list-group">
-        {players.map((player) => {
+        {players.map((player) => (
           <li className="list-group-item d-flex justify-content-between">
-            <span>
-              {player.name}
-            </span>
+            <span>{player.name}</span>
             <span>ELO: {player.points}</span>{" "}
-          </li>;
-        })}
+          </li>
+        ))}
       </ul>
-      <button type="button" class="btn btn-primary mt-3">
+      <button type="button" className="btn btn-primary mt-3">
         Add race stats
       </button>
     </div>
