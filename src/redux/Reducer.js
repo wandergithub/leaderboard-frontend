6225791caf694f354dd3741437614c32f1e0f8e6
wandergithub <wandergonzalez0@hotmail.com>
2023-03-28 12:39:41 -0400

    Implement redux store player creation

--- src/redux/Reducer.js
+++ src/redux/Reducer.js
@@ -2,12 +2,14 @@ import axios from "axios";
 
 // Actions
 const PLAYER_INDEX = "player/index";
+const PLAYER_CREATE = "player/create";
 // Reducer
 const Reducer = (state = { players: [], groups: [] }, action = {}) => {
   switch (action.type) {
     case PLAYER_INDEX:
       return { ...state, players: action.payload };
-
+    case PLAYER_CREATE:
+      return { ...state, players: [...players, action.payload] };
     default:
       return state;
   }
@@ -19,6 +21,11 @@ export const playersFetched = (players) => ({
   payload: players,
 });
 
+export const playerCreate = (player) => ({
+  type: PLAYER_CREATE,
+  payload: player,
+});
+
 // Thunks
 const BASE_URL = "http://localhost:3000";
 export const fetchPlayers = () => async (dispatch) => {
@@ -27,4 +34,13 @@ export const fetchPlayers = () => async (dispatch) => {
     dispatch(playersFetched(response.data));
   }
 };
+
+export const createPlayer = (name) => async (dispatch) => {
+  const data = { name: name };
+  const response = await axios.post(`${BASE_URL}/users`, data);
+  if (response) {
+    dispatch(playerCreate(response.data));
+  }
+};
+
 export default Reducer;
