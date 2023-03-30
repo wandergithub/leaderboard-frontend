e7fdc4fe87675296143ba9dcc2c952cdfa55488c
wandergithub <wandergonzalez0@hotmail.com>
2023-03-30 10:41:44 -0400

    Add redux store functionality to manage new race stats submitions and update players elo

--- src/redux/Reducer.js
+++ src/redux/Reducer.js
@@ -3,6 +3,7 @@ import axios from "axios";
 // Actions
 const PLAYER_INDEX = "player/index";
 const PLAYER_CREATE = "player/create";
+const RACE_STATS_ADD = "race_stats/add";
 // Reducer
 const Reducer = (state = { players: [], groups: [] }, action = {}) => {
   switch (action.type) {
@@ -10,6 +11,8 @@ const Reducer = (state = { players: [], groups: [] }, action = {}) => {
       return { ...state, players: action.payload };
     case PLAYER_CREATE:
       return { ...state, players: [...state.players, action.payload] };
+    case RACE_STATS_ADD:
+      return { ...state, players: action.payload };
     default:
       return state;
   }
@@ -24,6 +27,11 @@ export const playersFetched = (players) => ({
 export const playerCreated = (player) => ({
   type: PLAYER_CREATE,
   payload: player,
+});
+// Added new race stats update players
+export const raceStatsAdded = (players) => ({
+  type: RACE_STATS_ADD,
+  payload: players,
 });
 
 // Thunks
@@ -40,6 +48,16 @@ export const createPlayer = (name) => async (dispatch) => {
   const response = await axios.post(`${BASE_URL}/users`, data);
   if (response) {
     dispatch(playerCreated(response.data));
+  }
+};
+
+export const addRaceStats = (list) => async (dispatch) => {
+  // Takes a list of race results send to db to be updated and retrieve updated elo list
+  const data = { list };
+  const response = await axios.post(`${BASE_URL}`, data);
+  if (response) {
+    // Response should be a new list of players with new calculated elo
+    dispatch(raceStatsAdded(response.data));
   }
 };
 
