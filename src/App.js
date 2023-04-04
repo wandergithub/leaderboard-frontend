80d87c18460f09699c497b0927b688ee71b17a8b
wandergithub <wandergonzalez0@hotmail.com>
2023-04-04 17:07:50 -0400

    Add PublicBoard component route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Board from "./components/Board/Board";
 import { PlayerCreateForm } from "./components/PlayerForm/PlayerForm";
 import { fetchPlayers } from "./redux/Reducer";
+import PublicBoard from "./components/PublicBoard/PublicBoard";
 
 function App() {
   const dispatch = useDispatch();
@@ -18,6 +19,7 @@ function App() {
       <h1 className="m-5">SRL Leaderboard</h1>
       <Routes>
         <Route path="/" element={<Board />} />
+        <Route path="/public" element={<PublicBoard />} />
       </Routes>
       <PlayerCreateForm />
     </>
