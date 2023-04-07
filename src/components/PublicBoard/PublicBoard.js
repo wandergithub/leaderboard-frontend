1eee53db6c45d1b69932075a6ada5c503e4230e8
wandergithub <wandergonzalez0@hotmail.com>
2023-04-07 14:48:09 -0400

    Add public leaderboard

--- src/components/PublicBoard/PublicBoard.js
+++ src/components/PublicBoard/PublicBoard.js
@@ -2,29 +2,56 @@ import { useSelector } from "react-redux";
 
 export default function PublicBoard() {
   const players = useSelector((state) => state.players);
-  
+
   return (
-      <table className="table m-5" style={{ color: "white" }}>
+    <>
+      <h1>SRL Leader Board</h1>
+      <table
+        className="table m-5"
+        style={{ color: "#979899", fontFamily: "system-ui" }}
+      >
         <thead>
-            <tr>
-                <th scope="col">Rank</th>
-                <th scope="col">Player</th>
-                <th scope="col">Elo</th>
-                <th scope="col">Races #</th>
-            </tr>
+          <tr>
+            <th scope="col" className="t-title">
+              Rank
+            </th>
+            <th scope="col" className="t-title">
+              Player
+            </th>
+            <th scope="col" className="t-title">
+              Elo
+            </th>
+            <th scope="col" className="t-title">
+              Races #
+            </th>
+          </tr>
         </thead>
         <tbody>
-        {players.map((player, index) => (
-          <tr
-            key={player.id}
-          >
-            <td>{index + 1}</td>
-            <td>{player.name}</td>
-            <td>{player.points}</td>{" "}
-            <td>999</td>{" "}
-          </tr>
-        ))}
+          {players.map((player, index) => (
+            <tr
+              className={(index % 2 === 0 ? "r-1" : "r-2") + ""}
+              key={player.id}
+              style={{ color: "white" }}
+            >
+              <td
+                className={
+                  index === 0
+                    ? "gold"
+                    : index === 1
+                    ? "silver"
+                    : index === 2
+                    ? "bronce"
+                    : ""
+                }
+              >
+                {index + 1}
+              </td>
+              <td>{player.name}</td>
+              <td>{player.points}</td> <td>999</td>{" "}
+            </tr>
+          ))}
         </tbody>
       </table>
+    </>
   );
 }
